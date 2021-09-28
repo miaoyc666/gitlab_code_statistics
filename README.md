1f00558c1376548418b6dba4615ab3983330aa86
miaoyc <miaoyc1989@hotmail.com>
2021-09-28 14:17:15 +0800

    ====init====

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-#gitlab代码行数统计
+#Gitlab代码行数统计
 
+## 环境依赖
+* python3
+
+## 获取token
+1. 登录gitlab, 点击右上角个人信息Preferences； 
+2. 左侧选择`Access Tokens`, 输入token名称，选择权限范围（勾选api）和过期时间；
+3. 点击`Create personal access token` , 生成的个人账号token。
+
+## 统计代码量
+##### Usage
+`config.py`文件包含统计脚本需要用到的配置信息  
+`run.py`脚本为代码量统计脚本，使用语法：
+```
+python3 run.py
+```
+
+## 致谢
+gitlab_code_statistics初版参考了git-status项目。  
+感谢git-status项目原作者EightDoor，项目地址 ：https://gitee.com/EightDoor/git-status
---
+++ src/config.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : config.py
+Author       : miaoyc
+Create date  : 2021/9/28 1:38 下午
+Description  : 配置模板
+"""
+
+import datetime
+
+
+# gitlab仓库地址
+git_root_url = ""
+# 访问Token
+git_token = ""
+# 统计结果的存储目录
+export_path = "./dist"
+# 统计的时间区间-开始日期
+t_from = ""
+# 统计的时间区间-结束日期
+t_end = ""
+# 统计的时间区间-开始日期，datetime对象
+date_from = datetime.datetime.strptime(t_from, '%Y-%m-%d')
+# 统计的时间区间-结束日期，datetime对象
+date_end = datetime.datetime.strptime(t_end, '%Y-%m-%d')
+
+# 待统计的仓库列表
+valid_project = [
+]
