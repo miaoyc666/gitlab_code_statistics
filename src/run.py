d259d4a04eb920ebd73cb81d16c52f8a8ceb7570
miaoyc <miaoyc1989@hotmail.com>
2021-09-29 14:25:32 +0800

    ====bugfix===

--- src/run.py
+++ src/run.py
@@ -16,7 +16,7 @@ import threading
 import datetime
 from urllib import parse
 
-import config as config
+import config_miaoyc as config
 
 # 一个线程锁, 用于请求接口
 lock = threading.RLock()
@@ -102,7 +102,7 @@ class GitlabApiCount:
         # 等待所有线程结束
         for t in threads:
             t.join()
-
+        # 数据聚合
         final_commit_map = {}
         for key, project in self.totalMap.items():
             for author_email, detail in project.commit_map.items():
@@ -139,10 +139,11 @@ class GitlabApiCount:
         branch_map = {}
         # 主动获取master分支的提交
         detail_map = None
-        while not detail_map:
+        while detail_map is None:
             detail_map = self.get_commits(project_id, project_info.name, project_info.project_url, "master")
-            if detail_map:
-                print("Success get_commits: {0}->{1}".format(project_info.name, "master"))
+            if detail_map is not None:
+                pass
+                # print("Success get_commits: {0}->{1}".format(project_info.name, "master"))
         if detail_map:
             branch_map['master'] = detail_map
         for r3 in r2:
@@ -153,17 +154,14 @@ class GitlabApiCount:
             if r3['merged']:
                 continue
             #
-            key = int(time.time())
-            dic_plus_one(rate_limit, int(time.time()))
-            if rate_limit[key] >= 10:
-                time.sleep(1)
-                return -1
+            api_rate_limit()
 
             detail_map = None
-            while not detail_map:
+            while detail_map is None:
                 detail_map = self.get_commits(project_id, project_info.name, project_info.project_url, branch_name)
-                if detail_map:
-                    print("Success get_commits: {0}->{1}".format(project_info.name, branch_name))
+                if detail_map is not None:
+                    # print("Success get_commits: {0}->{1}".format(project_info.name, branch_name))
+                    pass
             # 将结果放到map里
             branch_map[branch_name] = detail_map
         final_commit_map = {}
@@ -215,15 +213,12 @@ class GitlabApiCount:
             "/api/v4/projects/%s/repository/commits?page=1&per_page=1000&ref_name=%s&since=%s&until=%s&private_token=%s" %
             (project_id, branch_name, since_date, until_date, config.git_token)
         )
-        r1 = requests.get(url)  # 请求url，传入header，ssl认证为false
+        r1 = requests.get(url)
         if r1.content == b'Retry later\n':
             # print("Exception get_commits: {0}->{1}".format(project_name, branch_name))
             return
         r2 = r1.json()  # 显示json字符串
-        if not r2:
-            return
         detail_map = {}
-
         for r3 in r2:
             commit_id = r3['id']
             if commit_id is None:
@@ -235,12 +230,15 @@ class GitlabApiCount:
                 continue
             # 这里开始获取单次提交详情
             detail = None
-            while not detail:
+            while detail is None:
                 detail = get_commit_detail(project_id, commit_id)
+                if isinstance(detail, int):
+                    detail = None if detail < 0 else CommitDetails()
                 if detail:
-                    print("Success get_commit_detail: {0}->{1}".format(project_id, commit_id))
-            if detail.total > 5000:
-                # 单次提交大于5000行的代码，可能是脚手架之类生成的代码，不做处理
+                    pass
+                    # print("Success get_commit_detail: {0}->{1}".format(project_id, commit_id))
+            # 过滤异常数据，单次提交大于5000行的代码，可能是脚手架之类生成的代码，不做处理
+            if detail.total == 0 or detail.total > 5000:
                 continue
             # 这里和主流程无关，是用来处理commit记录里的提交者，账号不规范的问题
             if detail.author_email in user_unknown:
@@ -270,13 +268,15 @@ def get_commit_detail(project_id, commit_id):
     api_rate_limit()
 
     # 开始真正的业务
-    url = '%s/api/v4/projects/%s/repository/commits/%s?private_token=%s' \
-          % (config.git_root_url, project_id, commit_id, config.git_token)
+    url = parse.urljoin(
+        config.git_root_url,
+        '/api/v4/projects/%s/repository/commits/%s?private_token=%s' % (project_id, commit_id, config.git_token)
+    )
+    # print(url)
     r1 = requests.get(url)  # 请求url，传入header，ssl认证为false
-
     if r1.content == b'Retry later\n':
-        # print("Exception get_commit_detail: {0}".format(commit_id))
-        return
+        print("Exception get_commit_detail: {0}".format(commit_id))
+        return -1
 
     r2 = r1.json()  # 显示json字符串
     author_name = r2['author_name']
@@ -284,9 +284,9 @@ def get_commit_detail(project_id, commit_id):
 
     stats = r2['stats']
     if 'Merge branch' in r2['title']:
-        return
+        return 1
     if stats is None:
-        return
+        return 2
     temp_mail = user_email_alias_mapping.get(author_email)
     if temp_mail is not None:
         author_email = temp_mail
@@ -296,8 +296,6 @@ def get_commit_detail(project_id, commit_id):
     additions = stats['additions']
     deletions = stats['deletions']
     total = stats['total']
-    # details = {'additions': additions, 'deletions': deletions, 'total': total, 'author_email': author_email,
-    #            'author_name': author_name}
     details = CommitDetails()
     details.additions = additions
     details.deletions = deletions
